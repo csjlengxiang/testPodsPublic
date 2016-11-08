8cf42103a777482a53c6ff0ddfc422bbaced0f81
csj <csjlengxiang@gmail.com>
2016-11-08 14:30:27 +0800

    add test class to demo

--- testPodsPublic/testPodsPublic/ViewController.m
+++ testPodsPublic/testPodsPublic/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "testClass.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    testClass * c = [testClass new];
+    [c sayHi];
 }
 
 
