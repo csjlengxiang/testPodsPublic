c44ed3e07b906fee778a4a943673d28eab2ea2a7
csj <csjlengxiang@gmail.com>
2016-11-08 14:23:20 +0800

    demo project init

---
+++ testPodsPublic/testPodsPublic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPodsPublic
+//
+//  Created by csj on 2016/11/8.
+//  Copyright © 2016年 csj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
